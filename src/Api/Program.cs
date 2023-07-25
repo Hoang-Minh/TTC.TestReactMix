47de9dd93e8a48119d8317e8727c96f742a782b9
Minh Nguyen <minh.nguyen@ocit.ocgov.com>
2023-07-25 12:47:43 -0700

    push changes

---
+++ src/Api/Program.cs
@@ -0,0 +1,64 @@
+using Microsoft.AspNetCore.Http.Headers;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddSpaStaticFiles(configuration => configuration.RootPath = "clientapp/build");
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllerRoute(name: "default", pattern: "{controller=Home }/{action=Index}/{id?}");
+var spaPath = "/";
+
+if(app.Environment.IsDevelopment())
+{
+    app.MapWhen(y => y.Request.Path.StartsWithSegments(spaPath), client =>
+    {
+        client.UseSpa(spa => spa.UseProxyToSpaDevelopmentServer("http://localhost:3333"));
+    });
+}
+else
+{
+    app.Map(new PathString(spaPath), client =>
+    {
+        client.UseSpaStaticFiles();
+        client.UseSpa(spa =>
+        {
+            spa.Options.SourcePath = "clientapp";
+
+            // add no-store header to index page to prevent deployment issues (prevent linking to old .js files)
+            // .js and other static resources are still cached by the browser
+            spa.Options.DefaultPageStaticFileOptions = new StaticFileOptions
+            {
+                OnPrepareResponse = ctx =>
+                {
+                    ResponseHeaders headers = ctx.Context.Response.GetTypedHeaders();
+                    headers.CacheControl = new Microsoft.Net.Http.Headers.CacheControlHeaderValue
+                    {
+                        NoCache = true,
+                        NoStore = true,
+                        MustRevalidate = true
+                    };
+                }
+            };
+        });
+    });
+}
+
+app.Run();
